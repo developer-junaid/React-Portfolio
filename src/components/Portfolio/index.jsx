dd26765ee26d3c7ef3ff91f7da8a52b9c1f34faa
developer-junaid <qjunaid623@gmail.com>
2021-09-22 08:13:43 +0500

    added material ui icons, created topbar layout

--- src/components/Portfolio/index.jsx
+++ src/components/Portfolio/index.jsx
@@ -2,7 +2,7 @@ import "./portfolio.scss";
 
 export default function Portfolio() {
   return (
-    <div className="portfolio">
+    <div className="portfolio" id="portfolio">
       <h2>Portfolio</h2>
     </div>
   );
--- src/components/Topbar/index.jsx
+++ src/components/Topbar/index.jsx
@@ -1,5 +1,35 @@
+// Styles
 import "./topbar.scss";
 
+// Icons
+import Person from "@material-ui/icons/Person";
+import Mail from "@material-ui/icons/Mail";
+
 export default function Topbar() {
-  return <div className="topbar"></div>;
+  return (
+    <div className="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="item-container">
+            <Person className="icon" />
+            <span>+44 888 12 41</span>
+          </div>
+          <div className="item-container">
+            <Mail className="icon" />
+            <span>qjunaid623@gmail.com</span>
+          </div>
+        </div>
+        <div className="right">
+          <div className="hamburger">
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
