4ab9239d7290b2bb6612e46c27d7b34f6ee256b2
developer-junaid <qjunaid623@gmail.com>
2021-09-24 09:30:46 +0500

    testimonials section compete

--- src/components/Testimonials/index.jsx
+++ src/components/Testimonials/index.jsx
@@ -1,9 +1,60 @@
 import "./testimonials.scss";
 
 export default function Testimonials() {
+  const data = [
+    {
+      id: 1,
+      name: "Tom Durden",
+      title: "Senior Developer",
+      img: "https://images.pexels.com/photos/1680172/pexels-photo-1680172.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/twitter.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem.",
+    },
+    {
+      id: 2,
+      name: "Alex Kalinski",
+      title: "Co-Founder of DELKA",
+      img: "https://images.pexels.com/photos/428321/pexels-photo-428321.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/youtube.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem recusandae perspiciatis ducimus vel hic temporibus. ",
+      featured: true,
+    },
+    {
+      id: 3,
+      name: "Martin Harold",
+      title: "CEO of ALBI",
+      img: "https://images.pexels.com/photos/3863793/pexels-photo-3863793.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+      icon: "assets/linkedin.png",
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat magnam dolorem",
+    },
+  ];
+
   return (
     <div className="testimonials" id="testimonials">
-      <h2>Testimonials</h2>
+      <h1>Testimonials</h1>
+      <div className="container">
+        {data.map((item) => (
+          <div
+            className={item.featured ? "card featured" : "card"}
+            key={item.id}
+          >
+            <div className="top">
+              <img
+                src="assets/right-arrow.png"
+                className="left"
+                alt={item.name}
+              />
+              <img src={item.img} alt="girl" className="user" />
+              <img src={item.icon} className="right" alt="" />
+            </div>
+            <div className="center">{item.desc}</div>
+            <div className="bottom">
+              <h3>{item.name}</h3>
+              <h4>{item.title}</h4>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
