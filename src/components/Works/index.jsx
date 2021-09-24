4d414f39ae1290098495d7e573e3e249c7a50445
developer-junaid <qjunaid623@gmail.com>
2021-09-24 09:12:50 +0500

    works section complete

--- src/components/Works/index.jsx
+++ src/components/Works/index.jsx
@@ -1,9 +1,82 @@
+import { useState } from "react";
+
+// Styles
 import "./works.scss";
 
+// Comp
 export default function Works() {
+  const data = [
+    {
+      id: "1",
+      icon: "./assets/mobile.png",
+      title: "Web Design",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ",
+      img: "https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/10/attachment_100040756-e1538485934255.jpeg?auto=format&q=60&fit=max&w=930",
+    },
+    {
+      id: "2",
+      icon: "./assets/globe.png",
+      title: "Mobile Application",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/e9/c9/2f/e9c92f7869d682a6fa5a97fb8a298f30.jpg",
+    },
+    {
+      id: "3",
+      icon: "./assets/writing.png",
+      title: "Branding",
+      desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+      img: "https://i.pinimg.com/originals/a9/f6/94/a9f69465d972a004ad581f245d6ad581.jpg",
+    },
+  ];
+
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  // Handlers
+  const handleClick = (way) => {
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+
+  // Return
   return (
     <div className="works" id="works">
-      <h2>Works</h2>
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((item) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="left-container">
+                  <div className="img-container">
+                    <img src={item.icon} alt={item.title} />
+                  </div>
+                  <h2>{item.title}</h2>
+                  <p>{item.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={item.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        alt="slider left arrow"
+        className="arrow left"
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        alt="slider right arrow"
+        className="arrow right"
+        onClick={() => handleClick("")}
+      />
     </div>
   );
 }
